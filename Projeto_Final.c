59e28a7d85891974a1cc9074c2a36a6b7dce3494
Clebson Ramos <clebsonramoss@gmail.com>
2025-02-20 01:20:19 -0300

    Versão 03. Inserção do menu de opções e da lógica de manipulação. Ainda existem bugs a serem corrigidos.

--- Projeto_Final.c
+++ Projeto_Final.c
@@ -15,7 +15,8 @@
 #define CONTADOR_LED 25
 #define PINO_MATRIZ_LED 7
 #define PINO_BOTAO_A 5
-#define LIMITE_SOM 90.0
+#define PINO_BOTAO_B 6
+#define PINO_JOYSTICK_Y 26
 #define I2C_PORTA i2c1
 #define ENDERECO 0x3C
 #define PINO_DISPLAY_SDA 14
@@ -37,8 +38,12 @@ LED_da_matriz leds[CONTADOR_LED];
 PIO maquina_pio;
 uint variavel_maquina_de_estado;
 
-static volatile bool estado_botao_A = false;
+static volatile bool estado_botao_A = false, estado_botao_B = false;
 static volatile uint32_t tempo_passado = 0;
+static volatile uint limite_som = 100;
+static volatile uint mestre = 0;
+int escolha_menu_1 = 1;
+bool controle_loop_menu_1 = true, controle_loop_sensibilidade = true;
 
 uint8_t matriz_leds_escutando[8][5] = {
 	{4, 6, 12, 18, 20},
@@ -51,8 +56,8 @@ uint8_t matriz_leds_escutando[8][5] = {
 	{5, 13, 17, 19, 21}
 }, num_frames_mle = 0;
 
-uint8_t coord_x_barra[10] = {4, 16, 28, 40, 52, 64, 76, 88, 100, 112};
-uint8_t barras_exibidas[17] = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+uint8_t coord_x_barra[10] = {14, 22, 35, 43, 56, 64, 77, 85, 98, 106};
+uint8_t barras_exibidas[10] = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
 
 ssd1306_t ssd;
 
@@ -62,6 +67,7 @@ void atribuir_cor_ao_led(const uint indice, const uint8_t r, const uint8_t g, co
 void limpar_o_buffer(void);
 void escrever_no_buffer(void);
 
+void alterar_sensibilidade_detector(void);
 void apresentacao_tela(uint ordem);
 void desenhar_barra(uint8_t x, uint8_t porcentagem);
 void funcao_de_interrupcao(uint pino, uint32_t evento);
@@ -69,6 +75,7 @@ void gravacao_do_som(void);
 void inicializacao_do_display(void);
 void inicializacao_dos_pinos(void);
 void manipulacao_matriz_leds(uint evento);
+void menu_opcoes(void);
 void movimentacao_da_fila(uint8_t porcentagem);
 bool tratamento_debounce(void);
 
@@ -78,16 +85,34 @@ int main(void){
     inicializacao_dos_pinos();
 	inicializacao_do_display();
 
+	inicializacao_maquina_pio(PINO_MATRIZ_LED);
+
     gpio_set_irq_enabled_with_callback(PINO_BOTAO_A, GPIO_IRQ_EDGE_FALL, true, &funcao_de_interrupcao);
+	gpio_set_irq_enabled_with_callback(PINO_BOTAO_B, GPIO_IRQ_EDGE_FALL, true, &funcao_de_interrupcao);
 
     while(true){
-		apresentacao_tela(2);
-        if(estado_botao_A){
-            gravacao_do_som();
-			estado_botao_A = !estado_botao_A;
-			apresentacao_tela(2);
-			sleep_ms(2000);
-        }
+		switch(mestre){
+			case 0: // Apresentação da tela inicial.
+				printf("Entrando na funcao apresentacao_tela(1) --- mestre [%d]\n", mestre);
+				apresentacao_tela(1);
+				break;
+			case 1: // Menu inicial.
+				if(controle_loop_menu_1){
+					printf("Entrando na funcao menu_opcoes() --- mestre [%d]\n", mestre);
+					menu_opcoes();
+				}
+				break;
+			case 2:
+				if(escolha_menu_1 == 2){
+					printf("Entrando na funcao alterar_sensibilidade_detector() --- mestre [%d]\n", mestre);
+					alterar_sensibilidade_detector();
+				}
+				break;
+			case 3: // Rotina de captura e monitoramento do som ambiente.
+				printf("Entrando na funcao gravacao_do_som() --- mestre [%d]\n", mestre);
+				gravacao_do_som();
+				break;
+		}
     }
 
     return 0;
@@ -143,14 +168,44 @@ void escrever_no_buffer(void){
 	sleep_us(100); // Espera 100us, sinal de RESET do datasheet.
 }
 
+//==================================================
+void alterar_sensibilidade_detector(void){
+	int nivel = 5;
+	char mensagem[4];
+	controle_loop_sensibilidade = true;
+
+	adc_select_input(0);
+
+	printf("Dentro da funcao alterar_sensibilidade_detector()\n");
+
+	while(controle_loop_sensibilidade){
+		ssd1306_fill(&ssd, false);
+		ssd1306_draw_string(&ssd, "SENSIBILIDADE", 4, 10);
+		itoa(limite_som, mensagem, 10);
+		ssd1306_draw_string(&ssd, mensagem, 50, 30);
+		ssd1306_draw_string(&ssd, "BOTAO B", 4, 50);
+		ssd1306_send_data(&ssd);
+		if(adc_read() <= 100){
+			limite_som -= 5;
+			if(limite_som < 5)
+				limite_som = 100;
+		}else if(adc_read() >= 4000){
+			limite_som += 5;
+			if(limite_som > 100)
+				limite_som = 5;
+		}
+		sleep_ms(100);
+	}
+}
 
 void apresentacao_tela(uint ordem){
 	ssd1306_fill(&ssd, false);
 
 	switch(ordem){
 		case 1:
-			ssd1306_draw_string(&ssd, "DETECTOR DE", 30, 20);
-			ssd1306_draw_string(&ssd, "SOM ATIVADO", 30, 40);
+			ssd1306_draw_string(&ssd, "DETECTOR DE", 20, 10);
+			ssd1306_draw_string(&ssd, "SOM ATIVADO", 20, 30);
+			ssd1306_draw_string(&ssd, "Pres botao A", 20, 50);
 			break;
 		case 2:
 			ssd1306_draw_string(&ssd, "SOM ATIPICO", 20, 10);
@@ -163,14 +218,11 @@ void apresentacao_tela(uint ordem){
 }
 
 void desenhar_barra(uint8_t x, uint8_t porcentagem){
-
 	uint16_t index = 0;
 	uint8_t barra[60], barra_cheia, barra_vazia;
 
 	barra_cheia = ALTURA_BARRA_TELA * porcentagem / 100;
 	barra_vazia = ALTURA_BARRA_TELA - barra_cheia;
-
-	printf("barra_cheia: %d\nbarra_vazia: %d\n\n", barra_cheia, barra_vazia);
 
 	for(uint i = 0; i < barra_vazia; i++)
 		barra[i] = 0x00;
@@ -186,6 +238,9 @@ void desenhar_barra(uint8_t x, uint8_t porcentagem){
 				ssd1306_pixel(&ssd, coord_x_barra[coord_y] + j, x + i, line & (1 << j));
 		}
 	}
+
+	ssd1306_hline(&ssd, 0, 127, 4 + (60 - 60 * limite_som / 100), true);
+
     ssd1306_send_data(&ssd);
 }
 
@@ -194,8 +249,32 @@ void funcao_de_interrupcao(uint pino, uint32_t evento){
         bool resultado_debounce = tratamento_debounce();
         if(resultado_debounce){
             estado_botao_A = !estado_botao_A;
+			switch(mestre){
+				case 0:
+					mestre = 1;
+					break;
+				case 1:
+					printf("Interrupcao do loop da funcao menu");
+					controle_loop_menu_1 = false;
+					if(escolha_menu_1 == 1){
+						mestre = 3;
+					}else if(escolha_menu_1 == 2){
+						mestre = 2;
+					}
+					break;
+			}
+			estado_botao_A = !estado_botao_A;
         }
-    }
+    }else if(pino == PINO_BOTAO_B && !estado_botao_B){
+		bool resultado_debounce = tratamento_debounce();
+		if(resultado_debounce){
+			estado_botao_B = !estado_botao_B;
+			controle_loop_sensibilidade = false;
+			escolha_menu_1 = 0;
+			mestre = 3;
+			estado_botao_B = !estado_botao_B;
+		}
+	}
 }
 
 void gravacao_do_som(void){
@@ -206,16 +285,22 @@ void gravacao_do_som(void){
 	ssd1306_fill(&ssd, true);
 	ssd1306_send_data(&ssd);
 
+	adc_select_input(2);
+
     while(controle_loop){
-		int som_captado = adc_read();
-		perturbacao_sonora = 100 * som_captado / 4096;
+		int som_captado = adc_read() - 2048;
+		if(som_captado < 0)
+			som_captado *= (-1);
+		perturbacao_sonora = 100 * som_captado / 2048;
 		movimentacao_da_fila(perturbacao_sonora);
-		sleep_ms(200);
+		//sleep_ms(200);
 		manipulacao_matriz_leds(1);
 		desenhar_barra(4, perturbacao_sonora);
-		if(perturbacao_sonora >= LIMITE_SOM)
+		if(perturbacao_sonora >= limite_som)
 			controle_loop = !controle_loop;
 	}
+	limpar_o_buffer();
+	escrever_no_buffer();
 }
 
 void inicializacao_do_display(void){
@@ -231,11 +316,15 @@ void inicializacao_do_display(void){
 void inicializacao_dos_pinos(void){
     adc_init();
     adc_gpio_init(PINO_MICROFONE);
-	adc_select_input(2);
+	adc_gpio_init(PINO_JOYSTICK_Y);
 
     gpio_init(PINO_BOTAO_A);
     gpio_set_dir(PINO_BOTAO_A, GPIO_IN);
     gpio_pull_up(PINO_BOTAO_A);
+
+	gpio_init(PINO_BOTAO_B);
+	gpio_set_dir(PINO_BOTAO_B, GPIO_IN);
+	gpio_pull_up(PINO_BOTAO_B);
 
 	// Inicialização do protocolo I2C em 400 kHz.
     i2c_init(I2C_PORTA, 400 * 1000);
@@ -246,14 +335,15 @@ void inicializacao_dos_pinos(void){
 }
 
 void manipulacao_matriz_leds(uint evento){
+	limpar_o_buffer();
+	escrever_no_buffer();
+
 	switch(evento){
 		case 1:
-			printf("Evento 1: Matriz de LEDs.\n");
-			limpar_o_buffer();
-			escrever_no_buffer();
 			for(uint i = 0; i < 5; i++){
 				atribuir_cor_ao_led(matriz_leds_escutando[num_frames_mle][i], 0, 0, INTENS_LEDS);
 			}
+			printf("Matriz de LEDs executada.\n");
 			escrever_no_buffer();
 			num_frames_mle++;
 			if(num_frames_mle == 8)
@@ -262,13 +352,48 @@ void manipulacao_matriz_leds(uint evento){
 	}
 }
 
+void menu_opcoes(void){
+	adc_select_input(0);
+
+	while(controle_loop_menu_1){
+		switch(escolha_menu_1){
+			case 1:
+				ssd1306_fill(&ssd, false);
+				ssd1306_draw_string(&ssd, "MENU OPCOES", 4, 10);
+				ssd1306_draw_string(&ssd, "Ativar detector", 4, 30);
+				ssd1306_draw_string(&ssd, "BOTAO A", 4, 50);
+				ssd1306_send_data(&ssd);
+				break;
+			case 2:
+				ssd1306_fill(&ssd, false);
+				ssd1306_draw_string(&ssd, "MENU OPCOES", 4, 10);
+				ssd1306_draw_string(&ssd, "Sensibilidade", 10, 30);
+				ssd1306_draw_string(&ssd, "BOTAO A", 4, 50);
+				ssd1306_send_data(&ssd);
+				break;
+		}
+
+		if(adc_read() <= 100){
+			escolha_menu_1--;
+			if(escolha_menu_1 < 1)
+				escolha_menu_1 = 2;
+		}else if(adc_read() >= 4000){
+			escolha_menu_1++;
+			if(escolha_menu_1 > 2)
+				escolha_menu_1 = 1;
+		}
+		sleep_ms(100);
+	}
+	printf("Fim da funcao menu_opcoes()\n");
+}
+
 void movimentacao_da_fila(uint8_t porcentagem){
 	uint8_t aux[17];
 
 	aux[0] = porcentagem;
-	for(uint i = 1; i < 17; i++)
+	for(uint i = 1; i < 10; i++)
 		aux[i] = barras_exibidas[i - 1];
-	for(uint i = 0; i < 17; i++)
+	for(uint i = 0; i < 10; i++)
 		barras_exibidas[i] = aux[i];
 }
 
--- inclusao/ssd1306.c
+++ inclusao/ssd1306.c
@@ -153,6 +153,10 @@ void ssd1306_draw_char(ssd1306_t *ssd, char c, uint8_t x, uint8_t y){
     index = (c - '0' + 1) * 8; // Adiciona o deslocamento necessário
   }else if(c >= 'a' && c <= 'z'){
     index = (c - 'a' + 37) * 8;
+  }else if(c == '-'){
+    index = (c + 63) * 8;
+  }else if(c == '.'){
+    index = (c + 64) * 8;
   }
   
   for (uint8_t i = 0; i < 8; ++i){
