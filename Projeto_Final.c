ed9c08f18631a72dbcb578cff3898aced5fba01a
Clebson Ramos <clebsonramoss@gmail.com>
2025-02-21 03:19:37 -0300

    Versão 05. Inserção das funcionalidades do PWM para controle de buzzers. Adaptação do código para função de ajuste de volume de buzzer. Bugs críticos ainda por resolver.

--- Projeto_Final.c
+++ Projeto_Final.c
@@ -4,6 +4,7 @@
 #include "hardware/adc.h"
 #include "hardware/clocks.h"
 #include "hardware/pio.h"
+#include "hardware/pwm.h"
 #include "hardware/timer.h"
 #include "inclusao/ssd1306.h"
 
@@ -17,6 +18,7 @@
 #define PINO_BOTAO_A 5
 #define PINO_BOTAO_B 6
 #define PINO_JOYSTICK_Y 26
+#define PINO_BUZINA_A 21
 #define I2C_PORTA i2c1
 #define ENDERECO 0x3C
 #define PINO_DISPLAY_SDA 14
@@ -38,12 +40,24 @@ LED_da_matriz leds[CONTADOR_LED];
 PIO maquina_pio;
 uint variavel_maquina_de_estado;
 
-static volatile bool estado_botao_A = false, estado_botao_B = false;
-static volatile uint32_t tempo_passado = 0;
-static volatile uint limite_som = 95;
+// Variáveis globais de controle geral do sistema.
+static volatile bool estado_botao_A = false; // Variável para registrar se o botão A foi pressionado.
+static volatile bool estado_botao_B = false; // Variável para registrar se o botão B foi pressionado.
+static volatile uint32_t tempo_passado = 0; // Variável para registrar o tempo passado para tratamento do bounce.
+static volatile int limite_som = 95; // Variável para registrar o limite de som (em % da amplitude do mic) que, caso ultrapassado, dispararia o alarme.
 static volatile uint mestre = 0;
 int escolha_menu_1 = 1;
-bool controle_loop_menu_1 = true, controle_loop_sensibilidade = true;
+bool controle_loop_menu_1 = true; // Controla a permanência no loop do menu de configuração.
+bool controle_loop_sensibilidade = true; // Controla a permanência no loop de ajuste de sensibilidade do detector de som.
+bool controle_loop_volume_alarme = true;
+bool controle_alarme_pwm = true; // Controla o loop de permanência do acionamento do alarme.
+
+// Variável global que armazena o número do slice associado ao pino que será usado como PWM.
+uint numero_slice;
+uint16_t duty_cycle = 0;
+uint16_t wrap = 35510;
+float divisor_de_clock = 4.0;
+int volume_alarme = 50; // Em porcentagem.
 
 uint8_t matriz_leds_escutando[8][5] = {
 	{4, 6, 12, 18, 20}, {3, 5, 7, 11, 19}, {2, 6, 8, 10, 14}, {1, 7, 9, 13, 15},
@@ -61,7 +75,9 @@ void atribuir_cor_ao_led(const uint indice, const uint8_t r, const uint8_t g, co
 void limpar_o_buffer(void);
 void escrever_no_buffer(void);
 
+void acionamento_do_alarme(void);
 void alterar_sensibilidade_detector(void);
+void alterar_volume_buzina(void);
 void apresentacao_tela(uint ordem, char mensagem_extra[]);
 void desenhar_barra(uint8_t x, uint8_t porcentagem);
 void funcao_de_interrupcao(uint pino, uint32_t evento);
@@ -75,7 +91,7 @@ bool tratamento_debounce(void);
 
 //-----FUNÇÃO PRINCIPAL-----
 int main(void){
-    stdio_init_all();
+	stdio_init_all();
     inicializacao_dos_pinos();
 	inicializacao_do_display();
 
@@ -91,22 +107,37 @@ int main(void){
 				apresentacao_tela(1, "00");
 				break;
 			case 1: // Menu inicial.
+				printf("controle_loop_menu_1 [%d] --- mestre [%d]\n", controle_loop_menu_1, mestre);
 				if(controle_loop_menu_1){
+					printf("Entrando na funcao menu_opcoes() --- mestre [%d]\n", mestre);
 					menu_opcoes();
+					printf("variavel escolha_menu_1 = %d\n", escolha_menu_1);
+					switch(escolha_menu_1){
+						case 1:
+							mestre = 2;
+							break;
+						case 2:
+							printf("Entrando na funcao alterar_sensibilidade_detector() --- mestre [%d]\n", mestre);
+							alterar_sensibilidade_detector();
+							break;
+						case 3:
+							printf("Entrando na funcao alterar_volume_buzina() --- mestre [%d]\n", mestre);
+							alterar_volume_buzina();
+							break;
+					}
+					//mestre = 2;
 				}
 				break;
-			case 2:
-				if(escolha_menu_1 == 2){
-					alterar_sensibilidade_detector();
-				}
-				break;
-			case 3: // Rotina de captura e monitoramento do som ambiente.
+			case 2: // Rotina de captura e monitoramento do som ambiente.
+				printf("Entrando na funcao gravacao_do_som() --- mestre [%d]\n", mestre);
 				gravacao_do_som();
-				mestre = 4;
+				mestre = 3;
 				break;
-			case 4: // Detecção de som atípico.
+			case 3: // Detecção de som atípico.
 				manipulacao_matriz_leds(3);
 				apresentacao_tela(2, "00");
+				printf("Entrando na funcao acionamento_do_alarme() --- mestre [%d]\n", mestre);
+				acionamento_do_alarme();
 				break;
 		}
     }
@@ -165,6 +196,16 @@ void escrever_no_buffer(void){
 }
 
 //==================================================
+void acionamento_do_alarme(void){
+	controle_alarme_pwm = true;
+	while(controle_alarme_pwm){
+		pwm_set_enabled(numero_slice, true);
+		sleep_ms(200);
+		pwm_set_enabled(numero_slice, false);
+		sleep_ms(200);
+	}
+}
+
 void alterar_sensibilidade_detector(void){
 	char mensagem[4];
 	controle_loop_sensibilidade = true;
@@ -187,6 +228,28 @@ void alterar_sensibilidade_detector(void){
 	}
 }
 
+void alterar_volume_buzina(void){
+	char mensagem[4];
+	controle_loop_volume_alarme = true;
+
+	adc_select_input(0);
+
+	while(controle_loop_volume_alarme){
+		itoa(volume_alarme, mensagem, 10);
+		apresentacao_tela(7, mensagem);
+		if(adc_read() <= 100){
+			volume_alarme -= 2;
+			if(volume_alarme < 0)
+				volume_alarme = 100;
+		}else if(adc_read() >= 4000){
+			volume_alarme += 2;
+			if(volume_alarme > 100)
+				volume_alarme = 0;
+		}
+		sleep_ms(100);
+	}
+}
+
 void apresentacao_tela(uint ordem, char mensagem_extra[]){
 	ssd1306_fill(&ssd, false);
 
@@ -199,12 +262,12 @@ void apresentacao_tela(uint ordem, char mensagem_extra[]){
 		case 2:
 			ssd1306_draw_string(&ssd, "SOM ATIPICO", 20, 10);
 			ssd1306_draw_string(&ssd, "DETECTADO", 30, 20);
-			ssd1306_draw_string(&ssd, "Press Botao A", 5, 50);
+			ssd1306_draw_string(&ssd, "Press Botao B", 5, 50);
 			break;
 		case 3:
 			ssd1306_draw_string(&ssd, "SENSIBILIDADE", 15, 10);
 			ssd1306_draw_string(&ssd, mensagem_extra, 50, 30);
-			ssd1306_draw_string(&ssd, "Press Botao B", 15, 50);
+			ssd1306_draw_string(&ssd, "Press Botao A", 15, 50);
 			break;
 		case 4:
 			ssd1306_draw_string(&ssd, "MENU OPCOES", 25, 10);
@@ -214,6 +277,16 @@ void apresentacao_tela(uint ordem, char mensagem_extra[]){
 		case 5:
 			ssd1306_draw_string(&ssd, "MENU OPCOES", 25, 10);
 			ssd1306_draw_string(&ssd, "Sensibilidade", 15, 30);
+			ssd1306_draw_string(&ssd, "Press Botao A", 15, 50);
+			break;
+		case 6:
+			ssd1306_draw_string(&ssd, "MENU OPCOES", 25, 10);
+			ssd1306_draw_string(&ssd, "Vol Alarme", 15, 30);
+			ssd1306_draw_string(&ssd, "Press Botao A", 15, 50);
+			break;
+		case 7:
+			ssd1306_draw_string(&ssd, "VOLUME ALARME", 15, 10);
+			ssd1306_draw_string(&ssd, mensagem_extra, 50, 30);
 			ssd1306_draw_string(&ssd, "Press Botao A", 15, 50);
 			break;
 	}
@@ -264,20 +337,17 @@ void funcao_de_interrupcao(uint pino, uint32_t evento){
             estado_botao_A = !estado_botao_A;
 			switch(mestre){
 				case 0:
+					printf("Acionamento botao A --- mestre [%d]\n", mestre);
 					mestre = 1;
 					break;
 				case 1:
+					printf("Acionamento botao A --- mestre [%d]\n", mestre);
+					printf("funcao_de_interrupcao() --> escolha_menu_1 [%d]\n", escolha_menu_1);
 					controle_loop_menu_1 = false;
-					if(escolha_menu_1 == 1){
-						mestre = 3;
-					}else if(escolha_menu_1 == 2){
+					controle_loop_sensibilidade = false;
+					controle_loop_volume_alarme = false;
+					if(escolha_menu_1 == 1)
 						mestre = 2;
-					}
-					break;
-				case 4:
-					mestre = 0;
-					controle_loop_menu_1 = true;
-					controle_loop_sensibilidade = true;
 					break;
 			}
 			estado_botao_A = !estado_botao_A;
@@ -286,9 +356,14 @@ void funcao_de_interrupcao(uint pino, uint32_t evento){
 		bool resultado_debounce = tratamento_debounce();
 		if(resultado_debounce){
 			estado_botao_B = !estado_botao_B;
-			controle_loop_sensibilidade = false;
-			escolha_menu_1 = 0;
-			mestre = 3;
+			printf("Acionamento botao B --- mestre [%d]\n", mestre);
+			if(mestre == 3){
+				mestre = 0;
+				controle_loop_menu_1 = true;
+				controle_loop_sensibilidade = true;
+				controle_loop_volume_alarme = true;
+				controle_alarme_pwm = false;
+			}
 			estado_botao_B = !estado_botao_B;
 		}
 	}
@@ -348,6 +423,14 @@ void inicializacao_dos_pinos(void){
     gpio_set_function(PINO_DISPLAY_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(PINO_DISPLAY_SDA);
     gpio_pull_up(PINO_DISPLAY_SCL);
+
+	// Inicialização do PWM para o buzzer A.
+	gpio_set_function(PINO_BUZINA_A, GPIO_FUNC_PWM); // Configura o pino do buzzer A como PWM.
+	numero_slice = pwm_gpio_to_slice_num(PINO_BUZINA_A); // Registra o número do slice associado a esse pino.
+	pwm_set_clkdiv(numero_slice, divisor_de_clock); // Configura o divisor de clock.
+	pwm_set_wrap(numero_slice, wrap); // Configura o período total.
+	pwm_set_gpio_level(PINO_BUZINA_A, wrap * volume_alarme / 100); // Configura o nível ativo (duty cycle).
+	pwm_set_enabled(numero_slice, false); // Desabilita o PWm. Será habilitado em outro trecho deste código.
 }
 
 void manipulacao_matriz_leds(uint evento){
@@ -399,7 +482,7 @@ void menu_opcoes(void){
 	adc_select_input(0);
 
 	// Necessária inicialização com o valor 1 ou 2 para evitar bug inicial de não aparecer as opções na tela.
-	escolha_menu_1 = 1;
+	//escolha_menu_1 = 1;
 
 	while(controle_loop_menu_1){
 		switch(escolha_menu_1){
@@ -409,19 +492,24 @@ void menu_opcoes(void){
 			case 2:
 				apresentacao_tela(5, "00");
 				break;
+			case 3:
+				apresentacao_tela(6, "00");
+				break;
 		}
 
 		if(adc_read() <= 100){
 			escolha_menu_1--;
 			if(escolha_menu_1 < 1)
-				escolha_menu_1 = 2;
+				escolha_menu_1 = 3;
 		}else if(adc_read() >= 4000){
 			escolha_menu_1++;
-			if(escolha_menu_1 > 2)
+			if(escolha_menu_1 > 3)
 				escolha_menu_1 = 1;
 		}
 		sleep_ms(100);
 	}
+	printf("menu_opcoes() --> controle_loop_menu_1 [%d]\n", controle_loop_menu_1);
+	printf("menu_opcoes() --> escolha_menu_1 = %d\n", escolha_menu_1);
 }
 
 void movimentacao_da_fila(uint8_t porcentagem){
